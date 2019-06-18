44da1facf9efecdeb8bfb255fdc61f9fa670a422
nodrygo <yvan.godin@gmail.com>
2019-06-18 19:58:33 +0200

    nouvelle version sans eSpeak mais avec des fichiers sons wav

--- README.md
+++ README.md
@@ -4,11 +4,21 @@ apprend  à écrire tes premiers mots
 
 ## BUT  
 Ce programme à été écrit sur la version 7.2  de Racket à destination de mes petits enfants ;-)
-fonctionne sur un Linux Mint 19 mais devrait pouvoir fonctionner sur MacOS ou Windows à la condition qu'une version du logiciel libre eSpeak soit utilisable dessus
+
+2 versions
+ * *ecrire.rk*t utilise eSpeak un sysnthétiseur vocal moyen    
+ * *ecrireWav.rkt* utile uniquement des sons .wav donc pas besoin de l'installation de **eSpeak**
+
+ecrire fonctionne sur un Linux Mint 19 mais devrait pouvoir fonctionner sur MacOS ou Windows à la condition qu'une version du logiciel libre eSpeak soit utilisable dessus    
+ecrireWav devrait marcher partout sans ajout     
+
+en cas d'erreur le mot s'affiche en fond clair pour aide    
+chaque mot sans erreur donne un bon point ;-)    
+
 
 ## Installation  
-installer [**eSpeak**](http://espeak.sourceforge.net/)  
-*si vous connaissez mieux en mode autonome et gratuit dites moi ;-)*   
+installer [**eSpeak**](http://espeak.sourceforge.net/) si besoin
+*si vous connaissez mieux comme synthé en mode autonome et gratuit dites moi ;-)*   
 
 puis intaller [**RACKET**](https://racket-lang.org/)   
 
@@ -16,18 +26,21 @@ Cloner ce repo dans un répertoire de votre convenance
 ``` git clone https://github.com/nodrygo/RktMaternelleCP ```  
 
 puis décompressez le fichier des imagettes dans le répertoire *RktMaternelleCP*  
-ceci doit crée un répertoire *img* indispensable  
+ceci doit crée un répertoire *img* indispensable    
+
+pour la version **ecrireWav** il faut décompresser le sons.7z dans le répertoire *RktMaternelleCP*    
+ceci doit crée un répertoire *sons* contenant tous les .wav
 
 # Utilisation   
-Ensuite avec *drracket* ou *gracket*  ouvrez et exécutez *ecrire.rkt*     
+Ensuite avec *drracket* ou *gracket*  ouvrez et exécutez *ecrire.rkt* ou *ecrireWav.rkt*   
 *vous pourrez le compiler pour le rendre autonome sur votre plateforme*   
 
 il est librement utilisable par qui le désire mais aux risques et périls des utilisateurs  
 
 la touche ***return*** permet de sélectionner un nouveau mot 
-les mots peuvent contenir des espaces   
+les mots peuvent contenir des espaces       
 les mot ne sont plus choisi aléatoirement mais un un cycle après mélange   
-la touche ***echap*** (***escape***) permet de quitter 
+la touche ***echap*** (***escape***) permet de QUITTER 
 
 
 ## Autres ressources
---
+++ ecrireWav.rkt
@@ -0,0 +1,238 @@
+#lang racket
+(require racket/system)
+(require racket/gui/base)
+(require racket/draw)
+(require racket/stream)
+; espeak -vfr+m2 -g 11  -p 55  -s 60 "écrit le mot chien avec un C un H un I  un E un N "
+(define wordslist  (shuffle(file->list "words.txt" )))
+(define selection '("" "appuie sur la touche ENTREE pour commencer " #f))
+(define nextword 0)
+(define currentword "appuie sur la touche ENTREE pour commencer" )
+(define currentpos 0 )
+(define blue-brush (new brush% [color "blue"]))
+(define blue-pen (make-pen #:color "blue"))
+(define green-pen (make-pen #:color "green"))
+(define font (make-font #:size 55  #:weight 'ultraheavy #:family 'decorative))
+(define fontnew (make-font #:size 25  #:weight 'ultraheavy #:family 'decorative))
+(define baseimg (make-bitmap 250 250))
+(define destimg (make-bitmap 250 250))
+(define bptimg (make-bitmap 100 100))
+(define bdc (send destimg make-dc))
+(define newwordgui 'none)
+;(define bptdc (send bptimg make-dc))
+(define endtxt "")
+(define help #f)
+(define err #f)
+(define playing #f)
+(define bpoints 0)
+(define proc #f)
+;(define cout  (open-output-file "cout" #:exists 'replace))
+;(define cerr  (open-output-file "cerr" #:exists 'replace ))
+(define progpath (find-executable-path "espeak"))
+
+(define (runspeak msg)
+    (define-values (s stdout stdin stderr) (subprocess #f #f #f 'new progpath "-vfr+m1" "-p 60" "-s 110" msg )) 
+    (set! proc s ))
+
+(define (killspeak)
+  (when proc (subprocess-kill proc #t))
+)
+
+;(runspeak "choisi un mot avec la touche ENTREE " )
+
+(define (curchar)
+    (string-ref currentword  currentpos))
+
+; load image and normalize scale in destimg
+(send bptimg  load-file "Bons_points.jpg")
+
+(define (loadimg name)
+  (when (file-exists? name)
+    (println name)
+       (send baseimg  load-file name)
+       (let [
+             (imgh (send baseimg get-height))
+             (imgw (send baseimg get-width))
+            ]
+            (send bdc draw-bitmap-section-smooth baseimg 0 0 250 250 0 0 imgw imgh)))
+)
+
+; handle mouse event 
+(define (handle-mouse ev)
+     (when (send ev button-down?)
+        (let [(mx (send ev get-x))
+              (my (send ev get-y))
+             ]
+          ;(println (format "~a ~a " mx my))
+          (when (send newwordgui in-region? mx my) (newword))
+          )
+))
+; handle key event
+(define (handle-ev ev)
+        (let [(key (send ev  get-key-code))
+             ]
+          (send ev get-key-code)
+          (handle-key key))
+)
+
+; handle keys 
+(define (handle-key key)
+    (cond 
+         ((eq? key 'escape) (killspeak)(exit 0))
+         ((eq? key #\return) (newword)(send canvas on-paint) )
+         (else (addkey key))
+    )
+)
+
+; draw word on canvas 
+(define (draw-word word dc px py)
+  (let-values ([( w h d v)  (send dc get-text-extent (car selection) font )  ])
+    (when help
+      (send dc set-text-foreground "Snow")(send dc set-text-mode 'transparent)
+      (send dc draw-text word  (+ w px 10) py )
+    )
+    (send dc set-text-foreground "Blue")(send dc set-text-mode 'transparent)
+    (send dc draw-text (car selection)   0 py )
+    (unless (>= 0 currentpos )(send dc draw-text (substring word 0  currentpos) (+ w px 10)  py ))
+  ))
+
+;set region
+(define (setnewwordregion dc)
+  (send dc set-origin 300 20)
+  (set! newwordgui (new region% [dc dc]))
+   (send newwordgui set-rounded-rectangle 0 0 160 60)
+   (send dc set-origin 0 0)
+  )
+;draw region new word
+(define (drawnewwordregion)
+  (let [(rdc(send newwordgui get-dc))   
+        ]
+  (send rdc set-pen blue-pen)
+  (send rdc set-brush blue-brush)
+  (send rdc draw-rounded-rectangle 0 0 160 60) 
+  (send rdc set-text-foreground "Red")
+  (send rdc draw-text "nouveau" 10  10 )
+))
+
+; paint handle
+(define (do-paint canvas dc)
+    (let* [
+         (canvasw (send canvas get-width))
+         (canvash (send canvas get-height))
+         (bptw 220)
+         (bpth 239)
+         (bptposx (- canvasw (+ bptw 5)))
+         (bptposy 5)
+         (px 50)
+         (bptxt  (format "~a" bpoints))
+         ;(py (/(send canvas get-height) 2))
+         (py 400)
+        ]
+  (send dc clear)
+  (send dc set-origin 300 20)
+  (send dc set-font fontnew) 
+  (drawnewwordregion) ;draw new word region
+  (send dc set-origin 0 0)
+  (send dc draw-bitmap destimg 	10 10 )
+  (send dc draw-bitmap bptimg   bptposx bptposy)
+  (send dc set-font font) 
+  (send dc set-pen blue-pen)
+  (draw-word currentword dc px (- py 100))
+  (draw-word (string-upcase currentword) dc px (+ py 5))
+  (send dc set-text-foreground "Red")
+  ; set text bonpoints pos
+  (let-values ([(tw th td tv)  (send dc get-text-extent bptxt font )  ])
+  (send dc draw-text bptxt  (-(+ bptposx(/ bptw 2) )(/ tw 2)) (-(+ bptposy (/ bpth 2))(/ th 2))))     
+  
+  (if err
+     (send dc set-text-foreground "Red")
+     (send dc set-text-foreground "Green")) 
+  (send dc draw-text endtxt   10 (+ py 100) )
+  
+ (unless playing
+   (send dc set-text-foreground "Green")
+   (send dc draw-text "Appuie sur ENTREE pour un nouveau mot"  0 (+ py 200) )
+)))
+; define windows
+(define myframe% (class frame%
+                 (define/override (on-subwindow-char target ev )(handle-ev ev))
+                 (super-new)))
+(define mainwin (new myframe% [label "Ecris un mot"][width 800 ][height 500]))
+(define game-canvas%
+  (class canvas% 
+    (define/override (on-event ev)
+       (handle-mouse ev))
+    (super-new)))
+(define canvas (new game-canvas%	[parent mainwin]
+                                [paint-callback do-paint]))
+
+; display window in full screen 
+(send mainwin  fullscreen #t)
+(send mainwin show #t)
+
+;change the current word
+(define (newword)
+  ;(killspeak)
+  (set! playing #t)
+  (set! currentpos 0)
+  (set! endtxt "")
+  (set! err #f)
+  (set! help #f)
+  ;; forget random use shuffle 
+  ;;(set! selection (list-ref wordslist  (random 0 (- (length wordslist ) 1 ))))
+  (set! selection (list-ref wordslist  nextword))
+  (set! nextword (+ 1 nextword))
+  (when (>=  nextword (length wordslist ))   (set! nextword 0))
+  (set! currentword (list-ref selection 1  ))
+  ; load img
+  (loadimg   (build-path  "img" (list-ref selection 2 )))
+  (send canvas on-paint)
+  (send canvas  refresh-now)
+    (play-sound (format "sons/~a.wav" currentword) #f)
+    (play-sound (format "sons/~a.wav" (curchar)) #f)
+ ; )    
+)
+
+(define (addkey k)
+
+  (unless (symbol? k )
+         (when (or(char-alphabetic? k)(char-whitespace? k) )
+           (printf "\n key :~a:   "  k)
+           (set! endtxt "")
+           (if (eq? k (string-ref currentword  currentpos))
+               (begin 
+                      (set! currentpos (+ 1 currentpos))
+                      (send canvas on-paint)
+                      (when  (< currentpos (string-length currentword))
+                        (if (char-whitespace? k)
+                          (play-sound (format "sons/espace.wav"  (curchar)) #f)   
+                          (play-sound (format "sons/~a.wav"  (curchar)) #f))
+                      )) 
+               (begin
+                      (set! err #t)
+                      (set! help #t)
+                      (set! endtxt (format "non il faut un ~a" (string-ref currentword   currentpos)))                 
+                      (send canvas on-paint)
+                      (play-sound "Pew.wav" #f)
+                      (if (char-whitespace? k)
+                          (play-sound (format "sons/espace.wav"  (curchar)) #f)   
+                          (play-sound (format "sons/~a.wav"  (curchar)) #f))
+                     
+           ))
+           
+           (when (>=  currentpos (string-length currentword))
+             (set! playing #f)
+             (if err
+                  (begin
+                   (set! endtxt "erreur dommage")
+                   (play-sound "sons/erreur.wav" #f)
+                   )
+                  (begin
+                   (set! endtxt "Super tu gagnes un bon point")
+                   (set! bpoints (+ 1 bpoints))
+                   (play-sound "sons/super.wav" #f)))
+             (send canvas on-paint)
+             )))
+)
+; start with new word 
+(setnewwordregion (send canvas get-dc))
